fe649eaf0258ebf3890ddd86d2f833445aa1f4d0
bobrepo <shadowiomr@gmail.com>
2025-09-19 19:07:49 +0530

    made the connection

--- game_files/main.c
+++ game_files/main.c
@@ -4,22 +4,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#define buffer_size 16
+#define PORT 5001
 
 int main() {
   int sock = socket(AF_INET, SOCK_DGRAM, 0);
   struct sockaddr_in serveraddr;
+  char *buffer = (char *)malloc(sizeof(char) * buffer_size);
   int si = sizeof(serveraddr);
-  memcpy(0, &serveraddr, sizeof(serveraddr));
+  memset(&serveraddr, 0, sizeof(serveraddr));
 
   serveraddr.sin_family = AF_INET;
   serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serveraddr.sin_port = htons(5050);
+  serveraddr.sin_port = htons(PORT);
 
-  bind(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+  bind(sock, (struct sockaddr *)&serveraddr, si);
 
-  listen(sock, 3);
-
-  nk = accept(sock, (struct sockaddr *)&serveraddr, &si);
+  recvfrom(sock, buffer, buffer_size, 0, NULL, 0);
+  printf("%s", buffer);
 
   return 0;
 }
